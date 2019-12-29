2cda0c43f644ea6b0e11ddbb1c1208f816c8f70b
vdehorta <vivien.de.horta@gmail.com>
2019-12-29 03:48:09 +0100

    stop opening new browser tab each time running front

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -71,6 +71,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
     new BrowserSyncPlugin({
       https: options.tls,
       host: 'localhost',
+      open: false,
       port: 9000,
       proxy: {
         target: `http${options.tls ? 's' : ''}://localhost:9060`,
