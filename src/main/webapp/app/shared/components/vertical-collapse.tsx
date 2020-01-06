9a709f6db960fcfeccc0e2508c9bf69db9a91782
vdehorta <vivien.de.horta@gmail.com>
2020-01-06 12:05:12 +0100

    add scroll behaviour to vertical-collapse with collapsing button

--- src/main/webapp/app/shared/components/vertical-collapse.tsx
+++ src/main/webapp/app/shared/components/vertical-collapse.tsx
@@ -16,7 +16,7 @@ const VerticalCollapse = (props) => {
   };
 
   return (
-    <Card className="vertical-collapse">
+    <Card className={isCollapsed ? "vertical-collapse active" : "vertical-collapse"}>
 
       <Button className="vertical-collapse-btn" size="sm" onClick={collapse}>
         <FontAwesomeIcon icon="arrow-right" className="menu-collapse-icon"/>
