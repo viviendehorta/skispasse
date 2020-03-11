ca85f43494f723b96214616310a887ad53328a00
vdehorta <vivien.de.horta@gmail.com>
2020-03-11 00:27:22 -0300

    SKIS22 add 'filterByCategories' method to NewsFactsResource, add integration tests and disabled CSRF in SecurityConfiguration to be able to test the app with REST client

--- src/main/java/vdehorta/config/SecurityConfiguration.java
+++ src/main/java/vdehorta/config/SecurityConfiguration.java
@@ -61,9 +61,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
-            .csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-        .and()
+            .csrf().disable()
             .addFilterBefore(corsFilter, CsrfFilter.class)
             .exceptionHandling()
             .authenticationEntryPoint(problemSupport)
