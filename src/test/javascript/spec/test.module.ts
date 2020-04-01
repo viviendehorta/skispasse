41d4ceb3275445855f296dc52a318164e6094a6a
vdehorta <vivien.de.horta@gmail.com>
2020-04-01 18:53:09 -0300

    SKIS35 delete administration and authentication parts of the app, with tests

--- src/test/javascript/spec/test.module.ts
+++ src/test/javascript/spec/test.module.ts
@@ -3,15 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { JhiLanguageService, JhiDataUtils, JhiDateUtils, JhiEventManager, JhiAlertService, JhiParseLinks } from 'ng-jhipster';
+import { JhiAlertService, JhiDataUtils, JhiDateUtils, JhiEventManager, JhiLanguageService, JhiParseLinks } from 'ng-jhipster';
 
-import { MockLanguageService, MockLanguageHelper } from './helpers/mock-language.service';
+import { MockLanguageHelper, MockLanguageService } from './helpers/mock-language.service';
 import { JhiLanguageHelper } from 'app/core/language/language.helper';
-import { AccountService } from 'app/core/auth/account.service';
-import { LoginModalService } from 'app/core/login/login-modal.service';
-import { MockAccountService } from './helpers/mock-account.service';
 import { MockActivatedRoute, MockRouter } from './helpers/mock-route.service';
-import { MockActiveModal } from './helpers/mock-active-modal.service';
 import { MockEventManager } from './helpers/mock-event-manager.service';
 
 @NgModule({
@@ -33,24 +29,12 @@ import { MockEventManager } from './helpers/mock-event-manager.service';
       useClass: MockEventManager
     },
     {
-      provide: NgbActiveModal,
-      useClass: MockActiveModal
-    },
-    {
       provide: ActivatedRoute,
       useValue: new MockActivatedRoute({ id: 123 })
     },
     {
       provide: Router,
       useClass: MockRouter
-    },
-    {
-      provide: AccountService,
-      useClass: MockAccountService
-    },
-    {
-      provide: LoginModalService,
-      useValue: null
     },
     {
       provide: JhiAlertService,
