ba4cfe5957f7db6be58cb7c59248fc50d9c326d5
vdehorta <vivien.de.horta@gmail.com>
2020-04-03 23:02:41 -0300

    SKIS33 import ol css cleaner way using jhipster vendor.scss file

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -72,7 +72,6 @@ module.exports = (options) => ({
             { from: './src/main/webapp/content/', to: 'content' },
             { from: './src/main/webapp/favicon.ico', to: 'favicon.ico' },
             { from: './src/main/webapp/manifest.webapp', to: 'manifest.webapp' },
-            { from: './node_modules/ol/ol.css', to: './content/css/ol.css' },
             // jhipster-needle-add-assets-to-webpack - JHipster will add/remove third-party resources in this array
             { from: './src/main/webapp/robots.txt', to: 'robots.txt' }
         ]),
