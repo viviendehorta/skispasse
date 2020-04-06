3cd95658f5a7f9857db9215f7873d955750c5c59
vdehorta <vivien.de.horta@gmail.com>
2020-04-06 13:46:19 -0300

    SKIS42 design account panel with user icon and login

--- src/main/webapp/app/shared/constants/role.constants.ts
+++ src/main/webapp/app/shared/constants/role.constants.ts
@@ -1,3 +1,3 @@
 export const ROLE_ADMIN = 'ROLE_ADMIN';
-export const ROLE_CONTRIBUTOR = 'ROLE_CONTRIBUTOR';
+export const ROLE_CONTRIBUTOR = 'ROLE_CONTRIB';
 export const ROLE_ANONYMOUS = 'ROLE_ANONYMOUS';
