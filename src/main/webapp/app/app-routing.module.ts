67f34ed4c9818cf33a30bcc3cef5d5c280566af6
vdehorta <vivien.de.horta@gmail.com>
2020-04-10 13:47:36 -0300

    SKIS52 add user management, retrieved from jhipster app

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -3,10 +3,27 @@ import { RouterModule } from '@angular/router';
 import { errorRoute } from './layouts/error/error.route';
 import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+
 const LAYOUT_ROUTES = [...errorRoute];
 
 @NgModule({
-  imports: [RouterModule.forRoot([...LAYOUT_ROUTES], { enableTracing: DEBUG_INFO_ENABLED })],
+  imports: [
+    RouterModule.forRoot(
+      [
+        {
+          path: 'admin',
+          data: {
+            authorities: ['ROLE_ADMIN']
+          },
+          canActivate: [UserRouteAccessService],
+          loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
+        },
+        ...LAYOUT_ROUTES
+      ],
+      { enableTracing: DEBUG_INFO_ENABLED }
+    )
+  ],
   exports: [RouterModule]
 })
 export class SkispasseAppRoutingModule {}
