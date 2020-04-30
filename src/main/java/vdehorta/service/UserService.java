58ca8464860c3d6f11e2a5c0816f8f9ba931add4
vdehorta <vivien.de.horta@gmail.com>
2020-04-30 15:15:32 -0300

    SKIS80 add unit test NowInstantTest to check that services, repositories and controllers never call Instant.now() but use ClockService instead

--- src/main/java/vdehorta/service/UserService.java
+++ src/main/java/vdehorta/service/UserService.java
@@ -293,7 +293,7 @@ public class UserService {
     @Scheduled(cron = "0 0 1 * * ?")
     public void removeNotActivatedUsers() {
         userRepository
-            .findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(Instant.now().minus(3, ChronoUnit.DAYS))
+            .findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(clockService.now().minus(3, ChronoUnit.DAYS))
             .forEach(user -> {
                 log.debug("Deleting not activated user {}", user.getLogin());
                 userRepository.delete(user);
