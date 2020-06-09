d0d29b93a24586bb274d5d494d725ce897e13a3b
vdehorta <vivien.de.horta@gmail.com>
2020-06-09 15:06:16 -0300

    SKIS136 clean spring configuration files and spring and maven profiles configuration

--- src/main/java/vdehorta/config/ProfileConstants.java
+++ src/main/java/vdehorta/config/ProfileConstants.java
@@ -3,8 +3,6 @@ package vdehorta.config;
 public interface ProfileConstants {
 
     // Spring profiles for development, test and production
-    /** Constant <code>SPRING_PROFILE_DEVELOPMENT="dev"</code> */
     String SPRING_PROFILE_DEVELOPMENT = "dev";
-    /** Constant <code>SPRING_PROFILE_PRODUCTION="prod"</code> */
-    String SPRING_PROFILE_PRODUCTION = "prod";
+    String SPRING_PROFILE_TEST = "test";
 }
