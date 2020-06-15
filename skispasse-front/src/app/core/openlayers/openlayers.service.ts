fed5729c5a75e0629bf42cd9dd0ee966fb13cc9d
vdehorta <vivien.de.horta@gmail.com>
2020-06-15 16:36:42 -0300

    SKIS146 simplify worldmap to only display a raster layer reading basic OpenMapTiles json data

--- skispasse-front/src/app/core/openlayers/openlayers.service.ts
+++ skispasse-front/src/app/core/openlayers/openlayers.service.ts
@@ -1,41 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
 import Feature from 'ol/Feature';
-import { Vector as VectorSource } from 'ol/source';
-import { Vector as VectorLayer } from 'ol/layer';
+import {TileJSON, Vector as VectorSource, XYZ} from 'ol/source';
+import {Tile, Vector as VectorLayer} from 'ol/layer';
 import View from 'ol/View';
 import {NewsFactNoDetail} from '../../shared/model/news-fact-no-detail.model';
 import {MappingService} from '../mapping/mapping.service';
+import Map from "ol/Map";
+import {fromLonLat} from "ol/proj";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class OpenLayersService {
-  constructor(private mappingService: MappingService) {}
+    constructor(private mappingService: MappingService) {
+    }
 
-  buildView(center: number[], zoom: number): View {
-    return new View({
-      center,
-      zoom
-    });
-  }
+    buildView(center: number[], zoom: number): View {
+        return new View({
+            center,
+            zoom,
+            constrainResolution: true
+        });
+    }
 
-  buildOSMTileLayer() {
-    return new TileLayer({
-      source: new OSM()
-    });
-  }
+    buildMarkerVectorLayer(allNewsFacts: NewsFactNoDetail[]): VectorLayer {
+        const vectorSource = new VectorSource({
+            features: this.mappingService.newsFactNoDetailsToFeatures(allNewsFacts)
+        });
+        return new VectorLayer({
+            source: vectorSource
+        });
+    }
 
-  buildMarkerVectorLayer(allNewsFacts: NewsFactNoDetail[]): VectorLayer {
-    const vectorSource = new VectorSource({
-      features: this.mappingService.newsFactNoDetailsToFeatures(allNewsFacts)
-    });
-    return new VectorLayer({
-      source: vectorSource
-    });
-  }
+    refreshLayerFeatures(features: Feature[], vectorLayer: VectorLayer) {
+        vectorLayer.getSource().clear();
+        vectorLayer.getSource().addFeatures(features);
+    }
 
-  refreshLayerFeatures(features: Feature[], vectorLayer: VectorLayer) {
-    vectorLayer.getSource().clear();
-    vectorLayer.getSource().addFeatures(features);
-  }
+    buildRasterLayer(jsonUrl: string): Tile {
+        return new Tile({
+          source: new TileJSON({
+            url: jsonUrl,
+            tileSize: 512,
+            crossOrigin: 'anonymous'
+          })
+        });
+    }
 }
--- skispasse-front/src/app/public/worldmap/worldmap.component.ts
+++ skispasse-front/src/app/public/worldmap/worldmap.component.ts
@@ -1,130 +1,49 @@
 import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import Map from 'ol/Map';
-import {Vector as VectorLayer} from 'ol/layer';
-import {Feature} from 'ol';
-import {Subscription} from 'rxjs';
-import {NewsFactNoDetail} from '../../shared/model/news-fact-no-detail.model';
-import {AccountService} from '../../core/auth/account.service';
-import {NewsFactService} from '../../core/newsfacts/news-fact.service';
-import {OpenLayersService} from '../../core/openlayers/openlayers.service';
-import {NewsFactDetailModalContentComponent} from './news-fact-detail-modal/news-fact-detail-modal.content.component';
-import {NewsCategorySelectionService} from '../../core/newscategory/news-category-selection.service';
-import {MappingService} from '../../core/mapping/mapping.service';
-import {ModalService} from '../../core/modal/modal.service';
-import {ROLE_ADMIN, ROLE_CONTRIBUTOR} from '../../shared/constants/role.constants';
-import {EventManager} from '../../core/events/event-manager';
+import {View} from 'ol';
+import {fromLonLat} from "ol/proj";
+import {OpenLayersService} from "../../core/openlayers/openlayers.service";
+import {NewsFactService} from "../../core/newsfacts/news-fact.service";
+import {NewsFactNoDetail} from "../../shared/model/news-fact-no-detail.model";
 
 @Component({
-  selector: 'skis-worldmap',
-  templateUrl: './worldmap.component.html',
-  styleUrls: ['./worldmap.scss']
+    selector: 'skis-worldmap',
+    templateUrl: './worldmap.component.html',
+    styleUrls: ['./worldmap.scss']
 })
-export class WorldmapComponent implements OnInit, AfterViewInit, OnDestroy {
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
-  private instance = this; // hack to be able to reach worldmapComponent instance in callback without word 'this'
+export class WorldmapComponent implements AfterViewInit, OnDestroy {
 
-  MAP_ID = 'worldmapPageNewsFactsMap';
-  ICON_PIXEL_CLICK_TOLERANCE = 3;
+    MAP_ID = 'worldmapPageNewsFactsMap';
+    newsFacts: NewsFactNoDetail[];
+    newsFactMap: Map;
 
-  private newsFactsMap: Map;
-  private newsFactMarkerLayer: VectorLayer;
+    constructor(
+        private newsFactService: NewsFactService,
+        private openLayersService: OpenLayersService
+    ) {}
 
-  newsFacts: NewsFactNoDetail[];
-
-  categorySelectionSubscription: Subscription;
-
-  constructor(
-    private accountService: AccountService,
-    private newsFactService: NewsFactService,
-    private openLayersService: OpenLayersService,
-    private mappingService: MappingService,
-    private modalService: ModalService,
-    private newsCategorySelectionService: NewsCategorySelectionService,
-    private eventManager: EventManager
-  ) {
-    this.newsFactsMap = null;
-    this.newsFactMarkerLayer = null;
-  }
-
-  ngOnInit() {
-    this.subscribeToNewsCategorySelectionEvents();
-  }
-
-  ngAfterViewInit() {
-    this.newsFactService.getAll().subscribe(newsFactNoDetails => {
-      this.newsFacts = newsFactNoDetails;
-      this.buildNewsFactsMap(this.newsFacts);
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.newsFactsMap) {
-      this.newsFactsMap.setTarget(null);
+    ngAfterViewInit() {
+      this.newsFactService.getAll().subscribe((newsFacts) => {
+        this.newsFacts = newsFacts;
+        this.buildNewsFactsMap()
+      })
     }
-    if (this.categorySelectionSubscription) {
-      this.eventManager.destroy(this.categorySelectionSubscription);
+
+    ngOnDestroy() {
     }
-  }
 
-  subscribeToNewsCategorySelectionEvents() {
-    this.categorySelectionSubscription = this.eventManager.subscribe('newsCategorySelectionChanged', event =>
-      this.onNewsCategorySelectionChanged(event)
-    );
-  }
-
-  buildNewsFactsMap(newsFacts: NewsFactNoDetail[]) {
-    const view = this.openLayersService.buildView([270000, 6250000], 1);
-    const oSMLayer = this.openLayersService.buildOSMTileLayer();
-    this.newsFactMarkerLayer = this.openLayersService.buildMarkerVectorLayer(newsFacts);
-
-    this.newsFactsMap = new Map({
-      layers: [oSMLayer, this.newsFactMarkerLayer],
-      target: this.MAP_ID,
-      view
-    });
-
-    // Behaviour when new fact markers are clicked : displaying detail modal
-    this.newsFactsMap.on('click', evt => {
-      this.newsFactsMap.forEachFeatureAtPixel(
-        evt.pixel,
-        (feature: Feature) => {
-          function showNewsFactDetail(worldmapInstance: WorldmapComponent, newsFactId: number): void {
-            worldmapInstance.showNewsFactDetail(newsFactId);
-          }
-
-          showNewsFactDetail(this.instance, feature.get('newsFactId'));
-          return true; // Returns true to stop feature iteration if there was several on the same pixel
-        },
-        {
-          layerFilter: layerCandidate => {
-            return layerCandidate === this.newsFactMarkerLayer;
-          },
-          hitTolerance: this.ICON_PIXEL_CLICK_TOLERANCE
-        }
-      );
-    });
-  }
-
-  showNewsFactDetail(newsFactId: number) {
-    this.newsFactService.getNewsFactDetail(newsFactId).subscribe(newsFactDetail => {
-      const modalRef = this.modalService.open(NewsFactDetailModalContentComponent, 'news-fact-detail-modal');
-      const detailComponentInstance = modalRef.componentInstance as NewsFactDetailModalContentComponent;
-      detailComponentInstance.setNewsFactDetail(newsFactDetail);
-    });
-  }
-
-  onNewsCategorySelectionChanged(event: { name: string; content: { categoryId: string; isSelected: boolean } }): void {
-    this.newsCategorySelectionService.setNewsCategorySelection(event.content.categoryId, event.content.isSelected);
-    const selectedCategoryIds = this.newsCategorySelectionService.getSelectedNewsCategoryIds();
-    const toShowNewsFacts = this.newsFactService.filterByCategoryIds(this.newsFacts, selectedCategoryIds);
-    this.openLayersService.refreshLayerFeatures(this.mappingService.newsFactNoDetailsToFeatures(toShowNewsFacts), this.newsFactMarkerLayer);
-  }
-
-  isAdmin() {
-    return this.accountService.hasAnyAuthority(ROLE_ADMIN);
-  }
-
-  isContributor() {
-    return this.accountService.hasAnyAuthority(ROLE_CONTRIBUTOR);
-  }
+    buildNewsFactsMap() {
+        this.newsFactMap = new Map({
+            layers: [
+                this.openLayersService.buildRasterLayer('https://api.maptiler.com/maps/basic/tiles.json?key=vRGImi4p2hajjVUCsIAE'),
+                this.openLayersService.buildMarkerVectorLayer(this.newsFacts)
+            ],
+            target: this.MAP_ID,
+            view: new View({
+                constrainResolution: true,
+                center: fromLonLat([0, 0]),
+                zoom: 2
+            })
+        });
+    }
 }
