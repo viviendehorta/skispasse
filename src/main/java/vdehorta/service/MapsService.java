0d33ca8cdf743d47936cda0cbc3957ef28da1cbf
vdehorta <vivien.de.horta@gmail.com>
2020-06-17 14:53:06 -0300

    SKIS150 improve map style fond and color : country=bold/dark, capital=bold/light, city=regular/dark, allow faster update of map style json in development

--- src/main/java/vdehorta/service/MapsService.java
+++ src/main/java/vdehorta/service/MapsService.java
@@ -1,11 +1,16 @@
 package vdehorta.service;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import vdehorta.domain.MapStyle;
 import vdehorta.repository.MapStyleRepository;
 import vdehorta.service.errors.MapStyleNotFoundException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Service class for managing news categories.
@@ -26,5 +31,20 @@ public class MapsService {
     public MapStyle getMapStyle() {
         log.debug("Getting json map style");
         return mapStyleRepository.findById(MAP_STYLE_ID).orElseThrow(() -> new MapStyleNotFoundException(MAP_STYLE_ID));
+//        return getResourceMapStyle("base-map.json"); //Enable in dev to modify styles directly in target/classes/mapstyles
+    }
+
+    private MapStyle getResourceMapStyle(String jsonFilename) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream jsonInputStream = classloader.getResourceAsStream("mapstyles/" + jsonFilename);
+
+        String json = null;
+        try {
+            json = IOUtils.toString(jsonInputStream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new MapStyle.Builder().id(MapsService.MAP_STYLE_ID).json(json).build();
     }
 }
