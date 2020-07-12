0eca7d260bdb57d2b8ee886c0225c43e95ac4ab3
vdehorta <vivien.de.horta@gmail.com>
2020-07-12 18:57:59 -0300

    SKIS169 add spring profiles 'local' and 'cloud' that uses mongo atlas database and use dev to set common properties used by both local and cloud run configuration

--- src/main/java/vdehorta/config/ProfileConstants.java
+++ src/main/java/vdehorta/config/ProfileConstants.java
@@ -3,4 +3,6 @@ package vdehorta.config;
 public interface ProfileConstants {
 
     String SPRING_PROFILE_DEV = "dev";
+    String SPRING_PROFILE_LOCAL = "local";
+    String SPRING_PROFILE_CLOUD = "cloud";
 }
