4a5e151b834e458d27a3a431d89d471f7a758a88
vdehorta <vivien.de.horta@gmail.com>
2020-07-27 14:58:31 -0300

    SKIS184 wip..... amend this commit with final one

--- skispasse-front/src/app/public/worldmap/newsfactmodal/news-fact-group/news-fact-group-modal.content.component.ts
+++ skispasse-front/src/app/public/worldmap/newsfactmodal/news-fact-group/news-fact-group-modal.content.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {NewsFactNoDetail} from "../../../shared/model/news-fact-no-detail.model";
-import {HTML_COLOR_BY_NEWS_CATEGORY} from "../../../core/map/news-category-color.constants";
-import {NewsCategoryService} from "../../../core/newscategory/news-category.service";
-import {NewsCategory} from "../../../shared/model/news-category.model";
-import {ILocationCoordinate} from "../../../shared/model/location-coordinate.model";
+import {NewsFactNoDetail} from "../../../../shared/model/news-fact-no-detail.model";
+import {NewsCategory} from "../../../../shared/model/news-category.model";
+import {NewsCategoryService} from "../../../../core/newscategory/news-category.service";
+import {HTML_COLOR_BY_NEWS_CATEGORY} from "../../../../core/map/news-category-color.constants";
+import {ILocationCoordinate} from "../../../../shared/model/location-coordinate.model";
 
 @Component({
     templateUrl: './news-fact-group-modal-content.component.html',
