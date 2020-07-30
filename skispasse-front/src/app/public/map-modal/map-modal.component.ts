9a028af166768cb24f161c7a88d3f80c12ffcc0a
vdehorta <vivien.de.horta@gmail.com>
2020-07-30 10:01:48 -0300

    SKIS184 go out of outlet route when closing map modal

---
+++ skispasse-front/src/app/public/map-modal/map-modal.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+declare var $: any;
+
+@Component({
+    selector: 'skis-map-modal',
+    templateUrl: './map-modal.component.html'
+})
+export class MapModalComponent implements OnInit {
+
+    ROUTED_MODAL_ID = 'routedModal';
+    mapModal: any;
+
+    constructor(
+        private router:Router
+    ) {}
+
+    ngOnInit() {
+        this.mapModal = $('#' + this.ROUTED_MODAL_ID);
+
+        //Go out from the outlet url on closing modal
+        this.mapModal.on('hidden.bs.modal', () => {
+            this.router.navigate([{ outlets: { modal: null } }]);
+        })
+    }
+
+    onModalOutletActivated() {
+        this.mapModal.modal('show');
+    }
+}
